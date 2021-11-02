c4902eb2e389e9cfcbb320666e93edc9b25457ac
kris.o <yajkakusta@gmail.com>
2021-11-02 13:41:02 +0500

    renamed files and create array project

--- array/obj/Debug/net6.0/array.AssemblyInfo.cs
+++ array/obj/Debug/net6.0/array.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("newfolder")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("newfolder")]
-[assembly: System.Reflection.AssemblyTitleAttribute("newfolder")]
+[assembly: System.Reflection.AssemblyProductAttribute("array")]
+[assembly: System.Reflection.AssemblyTitleAttribute("array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
