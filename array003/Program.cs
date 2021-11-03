b2427ec00e20e74dc48783e27e2936de21ec40fc
kris.o <yajkakusta@gmail.com>
2021-11-03 18:29:50 +0500

    create two methods with void: Fill and Print

---
+++ array003/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+/* создание методов
+заполнения массива
+печати массива на экран*/
+
+
+/*создать массив с методом void
+void-метод ничего не возвращает. в нём не используем оператор return
+ создание своего метода
+наименование метода - FillArray
+наименование аргумента - collection*/
+void FillArray(int[] collection) // метод, который заполняет массив. не используем запятую
+{
+  int length = collection.Length; // получить длину массива
+  int index = 0;
+  while (index<length)
+   {
+    collection[index] = new Random().Next(1,10);
+     index ++;
+   }
+}
+
+void PrintArray(int[] col) //метод, который печатает массив
+{
+   int count = col.Length;
+   int position = 0;
+   while (position<count)
+   {
+        Console.WriteLine(col[position]);
+        position ++;
+   }
+
+}
+int [] array = new int [10]; // создать новый массив, в котором будет n элементов
+FillArray(array); // метод, заполняющий массив
+PrintArray(array); // метод, выводящий массив
+
