5cfaf17217dd807e7427f1c6e668467efa0fedfe
kris.o <yajkakusta@gmail.com>
2021-11-05 11:35:08 +0500

    new projects

---
+++ array004/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Clear();
+
+/* возращаемый метод*/
+
+void FillArray(int[] collection) 
+{
+  int length = collection.Length; 
+  int index = 0;
+  while (index<length)
+   {
+    collection[index] = new Random().Next(1,10);
+     index ++;
+   }
+}
+
+void PrintArray(int[] col) 
+{
+   int count = col.Length;
+   int position = 0;
+   while (position<count)
+   {
+        Console.WriteLine(col[position]);
+        position ++;
+   }
+
+}
+
+int index(int[] collection, int find);
+ {
+int count = collection.Length;
+int index = 0;
+int position = 0;
+while (index<count)
+  {
+    if (collection[index] == find)
+    {
+        position = index;
+        
+    }
+    index ++;
+     
+   }
+   return position;
+ }
+int [] array = new int [10]; 
+FillArray(array); 
+PrintArray(array); 
+Console.WriteLine();
+int pos = Nindex(array, 4);
+Console.WriteLine(pos);
---
+++ dev2/Program.cs
@@ -0,0 +1,73 @@
+﻿/*Задача
+Имеется числовой массив A заполненный числами из отрезка [minValue; maxValue]. +
+Создать на его основе масив B, отбрасывая те, которые нарушают порядок
+
+    возрастания
+    элементы, больше 8
+    знакочередования*/
+
+
+//ввожу методы для дальнейшей работы
+
+void FillArray(int[] array) 
+{
+  int length = array.Length; 
+  int index = 0;
+  while (index<length)
+   {
+    array[index] = new Random().Next(-50,50);
+    index ++;
+    
+   }
+}
+void PrintArray(int[] array) 
+{
+   int count = array.Length;
+   int position = 0;
+   while (position<count)
+   {
+        Console.Write($"{array[position]} ");
+        position ++;
+   }
+
+}
+//создаю массив А со случайными числами
+int [] arrayA = new int [10];
+int minValue = new Random().Next(-50,10);
+int maxValue = new Random().Next(10,50);
+Console.Write("min number ");
+Console.WriteLine(minValue);
+Console.Write("max number ");
+Console.WriteLine(maxValue);
+Console.WriteLine("array А");
+int index = 0;
+while (index < 10)
+    {
+    arrayA[index] = new Random().Next (minValue, maxValue);
+    Console.Write($"{arrayA[index]} ") ;
+    index++;
+    }
+// создаю на его основе массив B, в котором исключены числа, нарушающие восрастание
+Console.WriteLine();
+int [] arrayAB = new int [10];
+FillArray(arrayAB); 
+Console.WriteLine("array AB");
+PrintArray(arrayAB);
+
+int current = arrayA[0]; //ввод элементов, не нарушающих порядка возрастания
+int counter = 0;
+for (int i = 0; i < arrayA.Length; i++)
+{
+    if (arrayA.Length>=current)
+    {
+     current = arrayA[i];
+     counter++;
+    }
+}
+int [] arrayB = new int [counter];
+Console.WriteLine("array B:");
+for (int cnt = 0;cnt < counter;cnt++)
+{
+    arrayB.Length = counter;
+}
+PrintArray(arrayB);
